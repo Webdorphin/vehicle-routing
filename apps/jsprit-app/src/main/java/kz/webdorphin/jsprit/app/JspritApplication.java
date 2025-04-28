fd24a0840f2ea94b5c11d0df6f34096b0fa88c1a
Aidar Shalakhmetov <33286223+WoodenBuddha@users.noreply.github.com>
2025-04-28 12:41:48 +0500

    Merge pull request #1 from Webdorphin/project-structure
    Initial project structure

---
+++ apps/jsprit-app/src/main/java/kz/webdorphin/jsprit/app/JspritApplication.java
@@ -0,0 +1,13 @@
+package kz.webdorphin.jsprit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspritApplication.class, args);
+	}
+
+}
